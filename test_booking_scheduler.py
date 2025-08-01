99ee4eab7c1643beb2963346d57c4996288b0b0e
yuchan <mo.yuchan.84@gmail.com>
2025-08-01 12:47:59 +0900

    [refactor] p18

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -5,20 +5,21 @@ from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
 from datetime import datetime
 
+NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+CUSTOMER = Customer("Fake name", "010-1234-5678")
+
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
     # arrange
-    not_on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
-    customer = Customer("Fake name", "010-1234-5678")
-    schedule = Schedule(not_on_the_hour, 1, customer)
+    schedule = Schedule(NOT_ON_THE_HOUR, 1, CUSTOMER)
     booking_scheduler = BookingScheduler(3)
     # act and assert
     with pytest.raises(ValueError):
         booking_scheduler.add_schedule(schedule)
 
 def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
-    on_the_hour = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
     customer = Customer("Fake name", "010-1234-5678")
-    schedule = Schedule(on_the_hour, 1, customer)
+    schedule = Schedule(ON_THE_HOUR, 1, customer)
     booking_scheduler = BookingScheduler(3)
     # act and assert
     booking_scheduler.add_schedule(schedule)
