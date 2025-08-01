0e1096d1f5c7e599d1efe6a56a93a40a4a34dbea
yuchan <mo.yuchan.84@gmail.com>
2025-08-01 12:44:34 +0900

    [feature] p17

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -16,7 +16,12 @@ def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가
         booking_scheduler.add_schedule(schedule)
 
 def test_예약은_정시에만_가능하다_정시인_경우_예약가능():
-    pass
+    not_on_the_hour = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+    customer = Customer("Fake name", "010-1234-5678")
+    schedule = Schedule(not_on_the_hour, 1, customer)
+    booking_scheduler = BookingScheduler(3)
+    # act and assert
+    booking_scheduler.add_schedule(schedule)
 
 def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생():
     pass
