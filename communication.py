173137f23bdc1b58c4827aa954695b969b538626
yuchan <mo.yuchan.84@gmail.com>
2025-08-01 12:59:27 +0900

    [feature] p24 dummy sender 생성

--- communication.py
+++ communication.py
@@ -6,3 +6,8 @@ class MailSender:
     def send_mail(self, schedule):
         if schedule.get_customer().get_email():
             print(f"Sending email to {schedule.get_customer().get_email()} for schedule at {schedule.get_date_time()}")
+
+
+class DummySmsSender(SmsSender):
+    def send(self, schedule):
+        print(f"Sending Dummy SMS to {schedule.get_customer().phone_number} for schedule at {schedule.get_date_time()}")
