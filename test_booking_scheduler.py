1864e4678940ac9d381bc8265931fa1a937a8437
yuchan <mo.yuchan.84@gmail.com>
2025-08-01 12:45:21 +0900

    [refactor] p17 (on_the_hour 변수 이름 변경)

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가():
     # arrange
-    not_on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+    on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
     customer = Customer("Fake name", "010-1234-5678")
-    schedule = Schedule(not_on_the_hour, 1, customer)
+    schedule = Schedule(on_the_hour, 1, customer)
     booking_scheduler = BookingScheduler(3)
     # act and assert
     with pytest.raises(ValueError):
